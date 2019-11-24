e2eeeb3f62601dd3c806f3235ac5167d8b13ad14
Magomed <magomed-gasanov1996@yandex.ru>
2019-11-25 02:04:10 +0300

    scetion-slide has been modified

--- js/main.js
+++ js/main.js
@@ -1,38 +1,10 @@
+
+
 $(function() {
-    // Owl Carousel
     let owl = $(".owl-carousel");
     owl.owlCarousel({
-        dots: true,
-        items: 1,
-        responsive: {
-        
-        520: {
-          items: 1.3
-        },    
-
-        600: {
-            items: 1.5
-        }, 
-
-        720: {
-          items: 2
-        },
-            
-        870: {
-          items: 2.5
-        },
-    
-        1300: {
-          items: 4
-        },
-
-        1600: {
-            items: 5
-        },
-
-        1850: {
-            items: 6
-        }
-      }
+        dots: false,
+        items: 6,
+        loop: true,
     });
   });
